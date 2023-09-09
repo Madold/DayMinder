a2d7d0bd51144d4877351289fe45d440dbb25304
Marco Portacio <marcoportacio4@gmail.com>
2023-09-09 10:52:13 -0500

    Finished initial setup

---
+++ app/src/main/java/com/markusw/dayminder/DayMinder.kt
@@ -0,0 +1,7 @@
+package com.markusw.dayminder
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class DayMinder: Application()
---
+++ app/src/main/java/com/markusw/dayminder/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.markusw.dayminder
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material3.Text
+import com.markusw.dayminder.ui.theme.DayMinderTheme
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            DayMinderTheme {
+                Text("hello world")
+            }
+        }
+    }
+}
+
