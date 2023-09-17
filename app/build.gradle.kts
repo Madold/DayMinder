f4d0073482d4d19003721606fe7c54cbbfad84af
Marco Portacio <marcoportacio4@gmail.com>
2023-09-17 11:31:12 -0500

    Finished setup of firebase crashlytics

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,6 +3,8 @@ plugins {
     id("org.jetbrains.kotlin.android")
     id("com.google.dagger.hilt.android")
     id("com.google.devtools.ksp")
+    id("com.google.gms.google-services")
+    id("com.google.firebase.crashlytics")
 }
 
 android {
