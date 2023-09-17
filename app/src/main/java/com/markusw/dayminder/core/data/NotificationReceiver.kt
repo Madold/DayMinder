04ac409224b3879b73a0d6e6b1a4ee7bdc239ace
Marco Portacio <marcoportacio4@gmail.com>
2023-09-17 12:26:14 -0500

    Changed reminder title and context text

--- app/src/main/java/com/markusw/dayminder/core/data/NotificationReceiver.kt
+++ app/src/main/java/com/markusw/dayminder/core/data/NotificationReceiver.kt
@@ -4,6 +4,7 @@ import android.app.NotificationManager
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import com.example.dayminder.R
 import com.markusw.dayminder.DayMinder
 import com.markusw.dayminder.core.domain.NotificationItem
 import com.markusw.dayminder.core.utils.NotificationBuilder
@@ -11,13 +12,12 @@ import com.markusw.dayminder.core.utils.NotificationBuilder
 class NotificationReceiver : BroadcastReceiver() {
     override fun onReceive(context: Context?, intent: Intent?) {
         val notificationTitle = intent?.getStringExtra(NotificationItem.TITLE) ?: return
-        val notificationMessage = intent.getStringExtra(NotificationItem.MESSAGE) ?: return
 
         val notification = NotificationBuilder.createNotification(
             context = context!!,
             chanelId = DayMinder.SCHEDULED_NOTIFICATIONS_CHANNEL_ID,
-            contentText = notificationMessage,
-            title = notificationTitle
+            contentText = notificationTitle,
+            title = context.getString(R.string.reminder)
         )
         val notificationManager = context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
         notificationManager.notify(0, notification)
