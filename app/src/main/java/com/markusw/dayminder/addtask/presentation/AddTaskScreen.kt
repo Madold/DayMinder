74dbafd992dd235088f3be5cf2fa157438fac207
Marco Portacio <marcoportacio4@gmail.com>
2023-09-17 12:16:13 -0500

    optimized imports

--- app/src/main/java/com/markusw/dayminder/addtask/presentation/AddTaskScreen.kt
+++ app/src/main/java/com/markusw/dayminder/addtask/presentation/AddTaskScreen.kt
@@ -33,7 +33,6 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.material3.Scaffold
